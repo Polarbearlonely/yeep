3d01f72da4da1dce34cce485bce133f5a800e1f6
Polarbearlonely <90439890+Polarbearlonely@users.noreply.github.com>
2022-04-07 13:41:04 +0800

    Add files via upload

--- triangle.c
+++ triangle.c
@@ -1,9 +1,10 @@
 main(){
     int n;
     scanf("%d",&n);
-    for(int i=n;i>0;i--){
+    for(int i=n;i>=0;i--){
         for(int j=i;j>0;j--){
-            printf("*");
+            if (i%2==0){printf("+");}
+            else {printf("*");}
         }
     printf("\n");
     }
